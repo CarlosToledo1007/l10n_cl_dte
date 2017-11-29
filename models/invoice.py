a59caaafd5f3f2e60ed1171bd5445cafb9e00f5f
Carlos Toledo <carlos.toledo1007@gmail.com>
2017-11-29 12:08:08 -0300

    Monto en Letra
    Se agrega el monto en letra al validar la factura

--- models/invoice.py
+++ models/invoice.py
@@ -921,6 +921,12 @@ www.sii.cl'''.format(folio, folio_inicial, folio_final)
         states={'draft': [('readonly', False)]},
     )
 
+    amount_letter = fields.Char(
+        string='Monto en letras',        
+        readonly=True,        
+    )
+    
+
     @api.multi
     def get_related_invoices_data(self):
         """
@@ -946,6 +952,7 @@ www.sii.cl'''.format(folio, folio_inicial, folio_final)
         for inv in self.with_context(lang='es_CL'):
             inv.sii_result = 'NoEnviado'
             inv.responsable_envio = self.env.user.id
+            inv.numero_to_letras()
             if inv.type in ['out_invoice', 'out_refund']:
                 if inv.journal_id.restore_mode:
                     inv.sii_result = 'Proceso'
@@ -1623,3 +1630,83 @@ www.sii.cl'''.format(folio, folio_inicial, folio_final)
             total_discount +=  (((l.discount or 0.00) /100) * l.price_unit * l.quantity)
         _logger.info(total_discount)
         return self.currency_id.round(total_discount)
+    
+    @api.one
+    def numero_to_letras(self):
+        numero = self.amount_total
+        
+        _logger.info(self.amount_total)
+    	indicador = [("",""),("MIL","MIL"),("MILLON","MILLONES"),("MIL","MIL"),("BILLON","BILLONES")]
+        entero = int(numero)
+        decimal = int(round((numero - entero)*100))
+        #print 'decimal : ',decimal 
+        contador = 0
+        numero_letras = ""
+        while entero >0:
+            a = entero % 1000
+            if contador == 0:
+                en_letras = self.convierte_cifra(a,1).strip()
+            else :
+                en_letras = self.convierte_cifra(a,0).strip()
+            if a==0:
+                numero_letras = en_letras+" "+numero_letras
+            elif a==1:
+                if contador in (1,3):
+                    numero_letras = indicador[contador][0]+" "+numero_letras
+                else:
+                    numero_letras = en_letras+" "+indicador[contador][0]+" "+numero_letras
+            else:
+                numero_letras = en_letras+" "+indicador[contador][1]+" "+numero_letras
+            numero_letras = numero_letras.strip()
+            contador = contador + 1
+            entero = int(entero / 1000)
+            
+        _logger.info(numero_letras)
+        numero_letras = numero_letras+" con " + str(decimal) +"/100"
+        print 'numero: ',numero
+        print numero_letras
+        self.amount_letter = numero_letras
+ 
+    def convierte_cifra(self,numero,sw):
+        lista_centana = ["",("CIEN","CIENTO"),"DOSCIENTOS","TRESCIENTOS","CUATROCIENTOS","QUINIENTOS","SEISCIENTOS","SETECIENTOS","OCHOCIENTOS","NOVECIENTOS"]
+        lista_decena = ["",("DIEZ","ONCE","DOCE","TRECE","CATORCE","QUINCE","DIECISEIS","DIECISIETE","DIECIOCHO","DIECINUEVE"),
+                        ("VEINTE","VEINTI"),("TREINTA","TREINTA Y "),("CUARENTA" , "CUARENTA Y "),
+                        ("CINCUENTA" , "CINCUENTA Y "),("SESENTA" , "SESENTA Y "),
+                        ("SETENTA" , "SETENTA Y "),("OCHENTA" , "OCHENTA Y "),
+                        ("NOVENTA" , "NOVENTA Y ")
+                    ]
+        lista_unidad = ["",("UN" , "UNO"),"DOS","TRES","CUATRO","CINCO","SEIS","SIETE","OCHO","NUEVE"]
+        centena = int (numero / 100)
+        decena = int((numero -(centena * 100))/10)
+        unidad = int(numero - (centena * 100 + decena * 10))
+        #print "centena: ",centena, "decena: ",decena,'unidad: ',unidad
+    
+        texto_centena = ""
+        texto_decena = ""
+        texto_unidad = ""
+    
+        #Validad las centenas
+        texto_centena = lista_centana[centena]
+        if centena == 1:
+            if (decena + unidad)!=0:
+                texto_centena = texto_centena[1]
+            else :
+                texto_centena = texto_centena[0]
+    
+        #Valida las decenas
+        texto_decena = lista_decena[decena]
+        if decena == 1 :
+            texto_decena = texto_decena[unidad]
+        elif decena > 1 :
+            if unidad != 0 :
+                texto_decena = texto_decena[1]
+            else:
+                texto_decena = texto_decena[0]
+        #Validar las unidades
+        #print "texto_unidad: ",texto_unidad
+        if decena != 1:
+            texto_unidad = lista_unidad[unidad]
+            if unidad == 1:
+                texto_unidad = texto_unidad[sw]
+        _logger.info("%s %s %s" %(texto_centena,texto_decena,texto_unidad))
+        return "%s %s %s" %(texto_centena,texto_decena,texto_unidad)
